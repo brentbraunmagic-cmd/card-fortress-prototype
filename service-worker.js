4989b364a53bcd8b9b0ebfb0933086b019812c21
brentbraunmagic-cmd <brentbraunmagic@gmail.com>
2026-08-17 01:01:32 -0400

    dashboard change

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'memory-fortress-v89';
+const CACHE_NAME = 'memory-fortress-v106';
 const OFFLINE_FILES = [
   './',
   './index.html',
@@ -28,6 +28,8 @@ const OFFLINE_FILES = [
   ,'./assets/dashboard-interceptor-windows-v1.png'
   ,'./assets/dashboard-raiders-training-v1.png'
   ,'./assets/dashboard-raiders-keypad-v1.png'
+  ,'./assets/dashboard-raiders-keypad-fortress-v2.png'
+  ,'./assets/dashboard-armored-shutter-v1.png'
   ,'./assets/menu-mode-panel-v1.png'
   ,'./assets/memory-fortress-splash-v1.png'
   ,'./assets/menu-report-frame-v1.png'
